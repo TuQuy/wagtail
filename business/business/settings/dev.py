acfcfd679adc82ca6d425f15ed335b5683c9d822
setup agn <tai.nguyen@aegona.com>
2023-08-14 17:57:00 +0700

    zzz

---
+++ business/business/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h)w_a1!@)j(!e)wdaf96@a43h=p8(iaz5)i33a1)s9xa&ol4st"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
