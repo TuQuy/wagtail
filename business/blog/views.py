8fe4760e769e4f481b4d869baf2214269b053a55
setup agn <tai.nguyen@aegona.com>
2023-08-16 17:05:11 +0700

    zz

--- business/blog/views.py
+++ business/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from business.home.models import HomePage
+
 # Create your views here.
+
+def home_viewq(request):
+    home_page = HomePage.objects.first()  # Lấy trang chủ, có thể thay đổi logic lấy trang chủ tùy theo cấu trúc của bạn
+    return render(request, 'home_page.html', {'home_page': home_page})
